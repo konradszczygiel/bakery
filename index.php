44f36fb4c5bbe0cb2db026e7e1744674f9ab282c
Konrad Szczyglowski <konrad.szczygiel@wp.pl>
2018-05-14 19:44:59 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>U cioci ciasta</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/u_cioci.css">
+</head>
+<body>
+
+
+<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Ciasta u cioci Ani">
+  Ciasta u cioci Ani
+</button>
+
+
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+</nav>
+
+
+
+
+
+<ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link active" href="lista_ciast.php">wprowadz liste ciast na sprzedaz</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="lista_klientow.php">lista klientow</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="lista_kontatow.php">kontakt z klientami</a>
+  </li>
+</ul>
+
+
+
+
+<ul class="list-group">
+  <li class="list-group-item justify-content-between">
+    NAZWA CIASTA
+    <span class="badge badge-default badge-pill">(ile ciasta zostalo)</span>
+  
+
+
+  <div class="input-group">
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Ile ciasta sprzedalas
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">1/4</a>
+          <a class="dropdown-item" href="#">1/3</a>
+          <a class="dropdown-item" href="#">1/2</a>
+          <a class="dropdown-item" href="#">2/3</a>
+          <a class="dropdown-item" href="#">3/4</a>
+          <div role="separator" class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+      </div>
+  </li>
+ 
+
+
+</body>
+</html>
